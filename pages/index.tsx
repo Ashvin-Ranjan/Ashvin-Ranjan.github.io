d35780dcfb15d8f708769c12500ea74b7230d3d4
Ashvin-Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2021-11-13 22:56:55 -0800

    fade in animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,100 @@
-import {ReactNode} from 'react';
-import Link from 'next/link';
-import Fade from 'react-reveal/Fade';
+import { ReactNode } from "react";
+import Link from "next/link";
 
 // MUI
 import Typography from "@material-ui/core/Typography";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: "flex",
-        alignContent: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-    },
-    letters: {
-        marginRight: "5%",
-        fontSize: "400%",
-    },
-    link: {
-        textDecoration: "none",
-        color: "white",
-    },
+  container: {
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  letters: {
+    marginRight: "5%",
+    fontSize: "400%",
+  },
+  title: {
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    fontSize: "200%",
+    marginBottom: "5%",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+  },
 }));
 
 export default function Home() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    function TypographyLink(props: {href: string, children: ReactNode, fade?: number}) {
-        return (
-            <Fade>
-                <Typography className={classes.letters}>
-                    <Link href={props.href}>
-                        <a className={classes.link}>
-                            {props.children}
-                        </a>
-                    </Link>
-                </Typography>
-            </Fade>
-        )
-    }
-
+  function TypographyLink(props: {
+    href: string;
+    children: ReactNode;
+    style?: Object;
+  }) {
     return (
-        <div className="App">
-            <header className="App-header">
-                <div /* className={classes.header} */>
-                    <div className={classes.container}>
-                        <TypographyLink href="/about">A</TypographyLink>
-                        <TypographyLink href="/stuff">s</TypographyLink>
-                        <TypographyLink href="/help">h</TypographyLink>
-                        <TypographyLink href="/.github">.</TypographyLink>
-                        <TypographyLink href="/videos">v</TypographyLink>
-                        <TypographyLink href="/internals">i</TypographyLink>
-                        <TypographyLink href="/N">n</TypographyLink>
-                    </div>
-                </div>
-            </header>
-        </div>
+      <Typography className={classes.letters} style={props.style}>
+        <a href={props.href} className={classes.link}>
+          {props.children}
+        </a>
+      </Typography>
     );
-};
+  }
+
+  let styles = [
+    {
+      opacity: 0,
+      animation: `fade 2000ms linear 0ms forwards`,
+    },
+  ];
+
+  for (let i = 0; i < 7; i++) {
+    styles.push({
+      opacity: 0,
+      animation: `fade 500ms linear ${2000 + i * 500}ms forwards`,
+    });
+  }
+
+  console.log(styles);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <div className={classes.title} style={styles[0]}>
+            Choose one
+          </div>
+          <div className={classes.container}>
+            <TypographyLink href="/about" style={styles[1]}>
+              A
+            </TypographyLink>
+            <TypographyLink href="/stuff" style={styles[2]}>
+              s
+            </TypographyLink>
+            <TypographyLink href="/help" style={styles[3]}>
+              h
+            </TypographyLink>
+            <TypographyLink href="/.github" style={styles[4]}>
+              .
+            </TypographyLink>
+            <TypographyLink href="/videos" style={styles[5]}>
+              v
+            </TypographyLink>
+            <TypographyLink href="/internals" style={styles[6]}>
+              i
+            </TypographyLink>
+            <TypographyLink href="/N" style={styles[7]}>
+              n
+            </TypographyLink>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
