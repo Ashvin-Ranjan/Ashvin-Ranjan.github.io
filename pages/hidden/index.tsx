69a6603d18bc184abbcc9a4c4a740e892d47157a
Ashvin-Ranjan <ranjanashvin@gmail.com>
2024-08-22 15:44:52 -0700

    Reset of the website

--- pages/hidden/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Hidden() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        Some things are better left hidden
-      </header>
-    </div>
-  );
-}
--- pages/stuff/rune.tsx
+++
@@ -1,8 +0,0 @@
-import TranslatePage from '../../components/stuff/TranslatePage';
-
-// Rune translate table
-import rune from '../../data/rune.json';
-
-export default function RuneTextGenerator() {
-  return <TranslatePage name='Rune Text Generator' dictionary={rune} />;
-}
